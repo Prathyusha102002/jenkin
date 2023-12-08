c7b4b0f801f07c444f5dfff843766014d6bd44bd
Ramireddy Prathyusha <ramireddy_prathyusha@thbs.india.com>
2023-12-08 14:51:47 +0530

    modified commit

--- BinarySearch.java
+++ BinarySearch.java
@@ -3,7 +3,7 @@ public class BinarySearch {
 	public static void main(String[] args) {
 
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int target = 6;
+        int target = 20;
         int result = binarySearch(arr, target);
         if (result != -1)
         {
